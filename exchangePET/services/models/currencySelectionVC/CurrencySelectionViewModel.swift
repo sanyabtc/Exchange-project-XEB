66123347980cdebcb070790fc7644b249723d597
sanyabtc <basalaevx14@mail.ru>
2025-04-19 22:22:04 +0300

    Рефакторинг MVVM

---
+++ exchangePET/services/models/currencySelectionVC/CurrencySelectionViewModel.swift
@@ -0,0 +1,30 @@
+//
+//  CurrencySelectionViewModel.swift
+//  exchangePET
+//
+//  Created by Александр Басалаев on 19.04.2025.
+//
+
+import Foundation
+
+final class CurrencySelectionViewModel {
+    private(set) var currencies: [Currency]
+    var selectedIndex: IndexPath?
+    var blockedCurrency: IndexPath?
+    
+    init(currencies: [Currency] = CurrencyRepository.shared.currencies) {
+        self.currencies = currencies
+    }
+    
+    func currency(at indexPath: IndexPath) -> Currency {
+        return currencies[indexPath.row]
+    }
+    
+    func numberOfCurrencies() -> Int {
+        return currencies.count
+    }
+    
+    func isCurrencyBlocked(at indexPath: IndexPath?) -> Bool {
+        return indexPath == blockedCurrency
+    }
+}
